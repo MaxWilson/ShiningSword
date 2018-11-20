8ac4da19a6665e29e81a7e9cb16a076a633a8f57
Max Wilson <wilson.max@gmail.com>
2018-11-20 08:16:45 -0800

    wip sketch end-to-end dataflow

--- ConsoleGame/Program.fs
+++ ConsoleGame/Program.fs
@@ -1,8 +1,23 @@
-﻿// Learn more about F# at http://fsharp.org
+// Learn more about F# at http://fsharp.org
 
 open System
+open Model
+open Operations
+open Interact
 
 [<EntryPoint>]
 let main argv =
+    let exec r =
+        let next = Intention(Queries.IntentionQuery.Query 22, fun i -> Operations.execute r [22, i])
+        let rec loop next =
+            match next with
+            | Intention(Queries.IntentionQuery.Query(id),_) -> printfn "What does %d want to do?" id
+            | _ -> failwithf "Not implemented: No match for %A" next
+            match Operations.Interact.trampoline next (Console.ReadLine()) with
+            | Some f ->
+                f()
+            | None ->
+                loop next
+        loop next
     printfn "Hello World from F#!"
     0 // return an integer exit code
