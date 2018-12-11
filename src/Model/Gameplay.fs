c1e358455ac9dc7b48feca0a7fc830d01d00e180
Max Wilson <maxw@microsoft.com>
2018-12-11 13:37:52 -0800

    Refactor to reduce template complexity in boilerplate

--- src/Model/Gameplay.fs
+++ src/Model/Gameplay.fs
@@ -9,16 +9,7 @@ let queryInteraction = Interaction.InteractionBuilder<Query, string>()
 let game() : Eventual<_,_,_> =
     let mutable target = rand 1000
     let mutable victories = 0
-    let rec victory(): Eventual<_,_,_> = queryInteraction {
-        victories <- victories + 1
-        let! keepGoing = Query.confirm (sprintf "Hooray! You got the right answer. You've played %d rounds and won them all. Want to keep going?" victories)
-        if keepGoing then
-            target <- rand 1000 // choose a new target
-            return! round 0
-        else
-            return ()
-        }
-    and round n : Eventual<_,_,_> = queryInteraction {
+    let rec round n : Eventual<_,_,_> = queryInteraction {
         let! guess = Query.number (sprintf "Guess a number! You have made %d guesses so far" n)
         if guess < target then
             do! Query.alert "Guess higher"
@@ -27,6 +18,12 @@ let game() : Eventual<_,_,_> =
             do! Query.alert "Guess lower"
             return! round (n+1)
         else
-            return! victory()
+            victories <- victories + 1
+            let! keepGoing = Query.confirm (sprintf "Hooray! You got the right answer. You've played %d rounds and won them all. Want to keep going?" victories)
+            if keepGoing then
+                target <- rand 1000 // choose a new target
+                return! round 0
+            else
+                return ()
         }
     round 0
