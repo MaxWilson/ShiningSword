1c1ec813629d28c66f56038f5e289b302b55e2b7
Max Wilson <maxw@microsoft.com>
2019-02-01 09:31:40 -0800

    wip on battle

--- src/Script1.fsx
+++ src/Script1.fsx
@@ -26,9 +26,30 @@ type Intention =
     | Attack of Attack option * Id option
     | Dodge
     | Move of Position
-type Effect = Log | Hit | Miss
+    | Kill of Attack option * Id option
+type Intentions = Intention list
+type ActorId = Id
+type TargetId = Id
+type Declaration = Declaration of ActorId * Intentions
+type Declarations = Declaration list
+type DeclarationStatus = Success | Failure | Incomplete of Declarations
+type Effect = Hit of ActorId * Effect | Miss of ActorId * TargetId | Move of ActorId * Position| Damage of TargetId * int | Chatter of ActorId option * string
+type Outcome = { effects: Effect list; status: DeclarationStatus; log: string list }
+type ExecOutcome = { effects: Effect list; unresolvedDeclarations: Declarations }
 
-let exec (intentions: (Id * Intention) list) (battle: Battle) : Effect list =
+let combineOutcomes (outcomes: Outcome list) : ExecOutcome =
+    { ExecOutcome.effects = outcomes |> List.collect (fun o -> o.effects);
+        unresolvedDeclarations = outcomes |> List.collect (function { status = Incomplete(i) } -> i | _ -> [])}
+
+let exec (declarations: Declarations) (battle: Battle) : ExecOutcome =
+    let exec (Declaration(id, intentions)) battle: Outcome =
+        Common.notImpl()
     Common.notImpl()
+
+combineOutcomes [
+    { effects = [Move(1, (0,0))]; status = Success; log = ["Bob charges over to the orc"] }
+    { effects = [Hit(1,(Damage(2,10))); Miss(1, 2); Chatter (Some 1, "Kreeeagh! Bob Bundolo!")]; status = Incomplete [Declaration(1, [Kill(None, Some 2)])]; log = [] }
+    { effects = [Miss(2, 1); Chatter (None, "The orc looks nervously around")]; status = Incomplete [Declaration(2, [Kill(None, None)])]; log = [] }
+    ]
 
 let apply effects (battle: Battle) : Battle = Common.notImpl()
