6a886bef31ad3414216e21b443717d4fbc0e97a6
Max Wilson <wilson.max@gmail.com>
2019-11-09 16:18:14 -0800

    Add FsCheck tests

--- ShiningSword.UnitTests/Ribbit.Parsing.Tests.fs
+++ ShiningSword.UnitTests/Ribbit.Parsing.Tests.fs
@@ -6,4 +6,7 @@ open Expecto
 let tests = testList "parsing" [
     testCase "My first test" <| fun _ ->
         Expect.isTrue true "I broke reality"
+    testProperty "Reverse of reverse of a list is the original list" <|
+      fun (xs:list<int>) -> List.rev (List.rev xs) = xs
+    testProperty "Addition is commutative" <| fun (x:int) (y:int) -> x + y = y + x
     ]
