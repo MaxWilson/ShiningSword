6730bb3ffcb6888dbc843ce4cd84326a31959b57
Max Wilson <wilson.max@gmail.com>
2019-11-11 12:52:52 -0800

    Start to add parsing to Ribbit

--- ShiningSword.UnitTests/Ribbit.Parsing.Tests.fs
+++ ShiningSword.UnitTests/Ribbit.Parsing.Tests.fs
@@ -1,22 +1,53 @@
 module Ribbit
 
+#if INTERACTIVE
+#I ".."
+#load "Common.fs"
+#load "Abstractions\Parsing.fs"
+#load "Domain\Dice.fs"
+#endif
+
 open Expecto
 open FsCheck
+open Common
+open Packrat
+open Domain.Dice
 
-let t = testList "parsing" [
-    testCase "max" <| fun _ ->
-        Expect.isTrue true "Truth"
-    ]
+
+#if INTERACTIVE
+let parseFail ((args: ParseArgs), _) = failwithf "Could not parse '%s'" args.input
+#else
+let parseFail ((args: ParseArgs), _) = Tests.failtestf "Could not parse %s" args.input
+#endif
 
 [<Tests>]
 let tests = testList "parsing" [
-    testCase "My first test" <| fun _ ->
-        Expect.isTrue true "I broke reality"
-    testProperty "Reverse of reverse of a list is the original list" <|
-      fun (xs:list<int>) -> List.rev (List.rev xs) = xs
-    testProperty "Addition is commutative" <| fun (x:int) (y:int) -> x + y = y + x
-    testProperty "additions should be positive" <| fun (x:bigint,y:bigint) ->
-        (x > 0I && y > 0I) ==>
-            ((x+y) > x && (x+y) > y)
-    t
+    testCase "Basic parsing" <| fun _ ->
+        // In this context, all external references are mapped to unit ()
+        let (|Term|_|) = Parse.(|Term|_|) (Parse.External.(|External|_|) ignore)
+        let parse x =
+            let adaptor : Parse.RosterAdaptor = {
+                isValidNamePrefix = fun _ -> false
+                tryNamePrefix = fun _ -> []
+                tryId = fun _ -> None
+                tryName = fun _ -> None
+                }
+            match ParseArgs.Init(x, adaptor) with
+            | Term(x, End) -> x
+            | v -> parseFail v
+        Expect.equal (parse "3d6+1") (Sum(Dice(3,6), Modifier 1)) "Should understand simple basic rolls"
+    testCase "Parse references" <| fun _ ->
+        let (|Term|_|) = Parse.(|Term|_|) (Parse.External.(|External|_|) id)
+        let parse x =
+            let adaptor : Parse.RosterAdaptor = {
+                isValidNamePrefix = "Bob".StartsWith
+                tryNamePrefix = fun x -> [0]
+                tryId = fun _ -> Some "Bob"
+                tryName = fun _ -> Some 0
+                }
+            match ParseArgs.Init(x, adaptor) with
+            | Term(x, End) -> x
+            | v -> parseFail v
+        Expect.equal (parse "3d6+Bob.STR") (Sum(Dice(3,6), External(0, "STR"))) "Should understand references to external things"
+        Expect.equal (parse "3d6+Bob's STR") (Sum(Dice(3,6), External(0, "STR"))) "Should understand references to external things"
     ]
--- src/Domain/Dice.fs
+++ src/Domain/Dice.fs
@@ -1,2 +1,97 @@
 module Domain.Dice
 
+open Common
+
+type Dice<'externalProperty> =
+    | Modifier of int
+    | Dice of number: int * kind: int
+    | External of 'externalProperty
+    | Sum of Dice<'externalProperty> * Dice<'externalProperty>
+    | Min of Dice<'externalProperty> * Dice<'externalProperty>
+    | Max of Dice<'externalProperty> * Dice<'externalProperty>
+
+type Fulfiller<'externalProperty> = 'externalProperty -> Dice<'externalProperty> // will resolve a structure with 'holes' in it to the same type of structure but with fewer 'holes' in it
+
+let instantiate fulfiller (dice:Dice<_>) : {| dice: Dice<_>; readyToRoll: bool |} =
+    {| dice = dice; readyToRoll = false |}
+
+let sample = function
+    | External _ -> shouldntHappen()
+    | Modifier n -> n
+
+#nowarn "40" // we're not doing anything funny at initialization-time, like calling functions in the ctor that rely on uninitialized members, so we don't need the warning
+module Parse =
+    open Packrat
+    let (|Mod|_|) = function
+        | Int(n, rest) -> Some(Modifier n, rest)
+        | _ -> None
+    let (|D|_|) = function
+        | Int(n, Str "d" (Int(d, rest))) -> Some(Dice(n, d), rest)
+        | Str "d" (Int(d, rest)) -> Some(Dice(1, d), rest)
+        | _ -> None
+    type RosterAdaptor = {
+                isValidNamePrefix: (string -> bool)
+                tryNamePrefix: string -> Id list
+                tryId: Id -> string option
+                tryName: string -> Id option
+            }
+    let (|Term|_|) (|External|_|) =
+        let rec (|Term|_|) =
+            pack <| function
+            | Term(d, Str "+" (Term(d', rest))) -> Some(Sum(d, d'), rest)
+            | External(e, rest) -> Some(External e, rest)
+            | Str "min(" (Term(d, Str "," (Term(d', Str ")" rest)))) -> Some(Min(d, d'), rest)
+            | Str "max(" (Term(d, Str "," (Term(d', Str ")" rest)))) -> Some(Max(d, d'), rest)
+            | D(d, rest) -> Some(d, rest)
+            | Mod(d, rest) -> Some(d, rest)
+            | _ -> None
+        (|Term|_|)
+    module External =
+        let (|Roster|_|) =
+            Packrat.ExternalContextOf<RosterAdaptor>
+        let (|ValidName|_|) =
+            // in this case we need to do something a little tricky: detect names by prefix
+            pack <| function
+            | Roster(roster) as ctx ->
+                match ctx with
+                | LongestSubstringWhere (roster.isValidNamePrefix) 30 (name, ctx) ->
+                    match roster.tryNamePrefix name with
+                    | [id] -> Some (id, ctx) // allow shortened names like El for Eladriel, as long as they are unambiguous (only one match)
+                    | matches ->
+                        // if there are multiple matches, try for an exact match. Don't want to disregard "skeleton 1" just because "skeleton 10" exists.
+                        matches |> List.tryPick(fun id -> match roster.tryId id with Some v when String.equalsIgnoreCase v name -> Some(id, ctx) | _ -> None)
+                | _ -> None
+            | _ -> None
+        let (|ValidNames|_|) =
+            let checkForIds ctx = function
+                | [] -> None
+                | ids -> Some(ids, ctx)
+            let (|Wildcard|_|) (roster: RosterAdaptor) = function
+                | Chars alphawhitespace (prefix, Char('*', ctx)) ->
+                    roster.tryNamePrefix prefix |> checkForIds ctx
+                | _ -> None
+            let (|Range|_|) (roster: RosterAdaptor) = function
+                | Chars alphawhitespace (prefix, Int(start, Str "-" (Int(finish, ctx)))) ->
+                    [
+                        for i in start..finish do
+                            let name = (prefix + (i.ToString()))
+                            match roster.tryName name with
+                            | Some id -> yield id
+                            | None -> ()
+                        ] |> checkForIds ctx
+                | _ -> None
+            // in this case we need to do something a little tricky: detect names by prefix
+            pack <| function
+            | Roster(roster) as ctx ->
+                match ctx with
+                | Wildcard roster (ids, ctx) -> Some(ids, ctx)
+                | Range roster (ids, ctx) -> Some(ids, ctx)
+                | ValidName(id, ctx) -> Some([id], ctx)
+                | _ -> None
+            | _ -> None
+        let (|External|_|) (createReference: (Id * string) -> 't) =
+            let (|CombatantProperty|_|) = pack <| function
+                | ValidName(id, Str "'s" (Word(propertyName, ctx))) -> Some((id, propertyName) |> createReference, ctx)
+                | ValidName(id, Str "." (Word(propertyName, ctx))) -> Some((id, propertyName) |> createReference, ctx)
+                | _ -> None
+            (|CombatantProperty|_|)
