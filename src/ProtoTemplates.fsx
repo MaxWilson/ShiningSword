7938d0ae670f5291a3ebca9d78af2185bc4328ca
Max Wilson <wilson.max@gmail.com>
2020-02-18 16:47:52 -0800

    wip

--- src/ProtoTemplates.fsx
+++ src/ProtoTemplates.fsx
@@ -13,5 +13,65 @@ let rollStats() =
         List.init 4 (thunk1 rand 6) |> List.sortDescending |> List.take 3 |> List.sum
     List.init 6 (ignore1 roll)
 
+type Stat = Str | Dex | Con | Int | Wis | CharData
+
+type Gender = Male | Female
+type Constraint =
+    | AtLeast of Stat * int
+    | Gender of Gender
+
+type NameGen = Gender -> string
+
+type Template = {
+    name: string
+    nameGen: NameGen
+    statPriorities: (Stat * int) list
+    constraints: Constraint list
+    }
+
+type Stats = {
+    str: int
+    dex: int
+    con: int
+    int: int
+    wis: int
+    cha: int
+    }
+
+type CharSheet = {
+    name: string
+    gender: Gender
+    template: Template
+    stats: Stats
+    }
 
 rollStats()
+
+let instantiate (template: Template) (nameGen: NameGen option) stats : CharSheet option =
+    let statsSatisfice (pri: (Stat * int) list) stats =
+        let pri = [(Str, 1); (Dex, 2); Con, 2; Str, 3]
+        let statsInOrderByPriority =
+            pri
+            |> Seq.distinctBy fst // break repeats arbitrarily
+            |> Seq.sortBy(ignore1 random.Next)
+            |> Seq.sortBy snd
+            |> Seq.map fst
+            |> List.ofSeq
+        let statIndex = function
+        let assignedStats =
+            statsInOrderByPriority
+            |>
+
+        ()
+    let genderSatisfice() =
+        template.constraints |> List.tryPick (function Gender g -> Some g | _ -> None) |> Option.defaultWith (fun () -> [Male; Female].[rand 2])
+    let gender = genderSatisfice()
+    let stats = statsSatisfice template stats
+    match stats with
+    | Some stats ->
+        { name = (defaultArg nameGen template.nameGen) gender
+          gender = gender
+          stats = stats
+          template = template
+          }
+    | None -> None
