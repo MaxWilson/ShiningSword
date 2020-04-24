d64e18083ad1a9c2efebbcde3d061a845632e73e
Max Wilson <wilson.max@gmail.com>
2020-04-23 23:46:12 -0700

    Working typeclasses via active patterns and anonymous records!

--- src/TypeClassExperiment.fsx
+++ src/TypeClassExperiment.fsx
@@ -1,14 +1,26 @@
 #I __SOURCE_DIRECTORY__
 #load @"Common.fs"
 
-type 'a EnumT =
-    abstract elements: 'a array
-type 'a OrdT =
-    abstract fromInt: int -> 'a
-    abstract toInt: 'a -> int
+type 't EnumW = {| elements: 't array |}
+// Following the Elm is Wrong methodology here by defining Ord based on fromInt/toInt.
+//     Ref: https://reasonablypolymorphic.com/blog/elm-is-wrong/
+// Yes, in real life you wouldn't define this type class with these members but that's not the point right now.
+//     We just want to show that you can write functions against these type-classes and automatically derive
+//     witnesses from other type class witnesses as needed.
+type 't OrdW = {|
+    fromInt: int -> 't
+    toInt: 't -> int
+    |}
 
-let sort<'t, 'tc when 'tc :> 't OrdT and 'tc :> 't EnumT> (tc:'tc) (items: 't array) =
-    items |> Array.map tc.toInt |> Array.sort |> Array.map tc.fromInt
+let inline (|EnumT|) (x: ^tc) =
+    {|
+        elements = (^tc : (member elements: ^t array) (x))
+    |}
+let inline (|OrdT|) (x: ^tc) =
+    {|
+        fromInt = (^tc : (member fromInt: (int -> ^t)) (x))
+        toInt = (^tc : (member toInt: (^t -> int)) (x))
+    |}
 
 type Color =
     | Red
@@ -33,23 +45,25 @@ type Color =
     | Orange
     | Blue
     with
-    static member enumW = {
-        new (Color EnumT) with
-            member this.elements = [|
-                Red; Yellow; Green; Brown; Scarlet; Black; Ochre; Peach; Ruby; Olive; Violet; Fawn;
-                Azure; Lemon; Russett; Grey; Purple; White; Pink; Orange; Blue
-            |] }
+    static member enumW: Color EnumW = {|
+        elements = [|
+            Red; Yellow; Green; Brown; Scarlet; Black; Ochre; Peach; Ruby; Olive; Violet; Fawn;
+            Azure; Lemon; Russett; Grey; Purple; White; Pink; Orange; Blue
+        |]
+    |}
 
-type 't OrdEnum =
-    inherit ('t EnumT)
-    inherit ('t OrdT)
+let inline sort (OrdT ord as tc) (items: 't array) =
+    items |> Array.map ord.toInt |> Array.sort |> Array.map ord.fromInt
 
-let derivingOrd (w: #('t EnumT)) = {
-    new ('t OrdEnum) with
-        member this.fromInt ix = w.elements.[ix]
-        member this.toInt v = w.elements |> Array.findIndex ((=) v)
-    interface ('t EnumT) with
-        member this.elements = w.elements
-    }
+let inline derivingOrd (witness: 't EnumW) = {|
+        witness with
+            fromInt = fun ix -> witness.elements.[ix]
+            toInt = fun v -> witness.elements |> Array.findIndex ((=) v)
+    |}
 
-[|Grey; Red; White; Blue; Pink|] |> sort (derivingOrd Color.enumW)
+[|Grey; Red; White; Blue; Pink|] |> sort (derivingOrd Color.enumW) // we can sort based on a derived typeclass!
+[|8.0;3.3; 3.14; -9.|] |> sort ({| toInt = (fun (f: float) -> int f); fromInt = (fun (i:int) -> float i) |})
+// We can supply an explicit witness for floats
+type Mood = Happy | Sad | Angry
+[|Angry; Sad; Sad; Happy|] |> sort (derivingOrd {| elements = [| Happy; Sad; Angry |] |}) // proof that it's really generic for all Enums
+
