333bfaebff1fed38c82c2c05dccbe4c9c6a38bf8
Max Wilson <wilson.max@gmail.com>
2020-04-28 15:52:40 -0700

    Starting to fill out arch

--- src/Arch.fs
+++ src/Arch.fs
@@ -1,1 +1,21 @@
 module Arch
+
+type Id = int
+type Roll = exn
+type Expression = exn
+type Statement = exn
+type PropertyId = Id
+type PropertyDefinition = exn
+type Ref = exn
+type Value = exn
+type PropertyKey = Id * PropertyId
+type GameState = {
+    lastCreatureId: Id option
+    properties: Map<PropertyKey, Value>
+    propertyDefinitions: PropertyDefinition array
+    }
+type IntermediateGameState = {
+    game: GameState
+    processingQueue: Ref list
+    }
+type IterateToFixedPoint = IntermediateGameState -> GameState
