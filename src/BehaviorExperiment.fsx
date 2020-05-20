c2b9312adab0cc22ad4567be9884184efe2b57b2
Max Wilson <wilson.max@gmail.com>
2020-05-20 16:39:57 -0700

    My initial stab at effects. Handles output.

--- src/BehaviorExperiment.fsx
+++ src/BehaviorExperiment.fsx
@@ -4,27 +4,34 @@
 open Optics
 open Optics.Operations
 
-type Todo() =
-    do
-    failwith "Not implemented"
-type Ref = Id
-type Value = string
-type Awaitable<'Ref, 't> = Await of 'Ref | InProgress | Done of 't
-type Thing = { value: Value }
-type Model = {
-    Blocking: Map<Ref, Ref list>
-    Blocked: Map<Ref, Ref list>
-    Queue: Ref list
-    Things: Map<Ref, Awaitable<Ref * Model -> Model, Thing>>
-    }
+let inv f = f ()
+type Effect<'t> =
+    | Log of string * (unit -> 't Effect)
+    | Result of 't
 
-let tryAwait (lens: 'addr -> Lens<_, _>) lookup queue myAddress (state: Model) =
-    let b = state |> read (lens myAddress)
-    match b with
-    | Done _ | InProgress ->
-        state
-    | Await (ref, progress: 'continuation) ->
-        match lookup ref with
-        | Done v ->
-            state |> write (lens myAddress) InProgress |> queue myAddress progress
-        | _ -> state
+let rec bind f = function
+    | Log(msg, cont) ->
+        Log(msg, fun() -> cont() |> bind f)
+    | Result v -> f v
+type EffectBuilder() =
+    member this.Bind(effect, f) = bind f effect
+    member this.Return e = Result e
+let effect = EffectBuilder()
+
+let handle e =
+    let rec loop accum = function
+        | Result v -> v, List.rev accum
+        | Log(msg, f) -> loop (msg::accum) (inv f)
+    loop [] e
+let log msg = Log(msg, fun() -> Result())
+
+let rec fib = function
+    | 0 | 1 -> effect { return 1 }
+    | n -> effect {
+        let! a = fib (n-1)
+        let! b = fib (n-2)
+        do! (log (sprintf "%d(%d) |X| %d(%d) ==> %d(%d)" (n-1) a (n-2) b n (a+b)))
+        return (a + b)
+        }
+
+fib 10 |> handle
