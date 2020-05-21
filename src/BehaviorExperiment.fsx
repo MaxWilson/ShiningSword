c2fbe668051537a3ff375fbfd7de49c4a5a0d606
Max Wilson <wilson.max@gmail.com>
2020-05-21 15:48:22 -0700

    Add tryExtractResult, extractResult, extract

--- src/BehaviorExperiment.fsx
+++ src/BehaviorExperiment.fsx
@@ -16,14 +16,34 @@ let rec bind f = function
 type EffectBuilder() =
     member this.Bind(effect, f) = bind f effect
     member this.Return e = Result e
+let log msg = Log(msg, fun() -> Result())
+let logf msg fmt = Printf.kbprintf msg
+
 let effect = EffectBuilder()
+
+let tryExtractResult e =
+    match e with
+    | Result v -> Some v
+    | _ -> None
+
+// asserts that we're already at a fixed point
+let extractResult e =
+    match tryExtractResult e with
+    | Some v -> v
+    | None -> failwith "That effect hasn't finished all of its side effects yet"
+
+let extract e =
+    let rec loop accum = function
+        | Result v -> v, List.rev accum
+        | Log(msg, cont) ->
+            cont() |> loop (msg::accum)
+    loop [] e
 
 let handle e =
     let rec loop accum = function
         | Result v -> v, List.rev accum
         | Log(msg, f) -> loop (msg::accum) (inv f)
     loop [] e
-let log msg = Log(msg, fun() -> Result())
 
 let rec fib = function
     | 0 | 1 -> effect { return 1 }
@@ -35,3 +55,5 @@ let rec fib = function
         }
 
 fib 10 |> handle
+fib 10 |> extractResult
+fib 10 |> extract
