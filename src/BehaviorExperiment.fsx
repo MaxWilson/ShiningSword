142364042a8770d78e895b30dc20f7a0e86cce86
Max Wilson <wilson.max@gmail.com>
2020-05-21 16:37:25 -0700

    Attempt to add capabilities and input. Something not quite right.

--- src/BehaviorExperiment.fsx
+++ src/BehaviorExperiment.fsx
@@ -5,19 +5,23 @@ open Optics
 open Optics.Operations
 
 let inv f = f ()
+type Input = string
 type Effect<'t> =
     | Log of string * (unit -> 't Effect)
+    | Read of query: string * continuation: (Input -> 't Effect)
     | Result of 't
 
 let rec bind f = function
     | Log(msg, cont) ->
         Log(msg, fun() -> cont() |> bind f)
+    | Read(query, cont) ->
+        Read(query, fun input -> cont input |> bind f)
     | Result v -> f v
 type EffectBuilder() =
     member this.Bind(effect, f) = bind f effect
     member this.Return e = Result e
 let log msg = Log(msg, fun() -> Result())
-let logf msg fmt = Printf.kbprintf msg
+let logf fmt = Printf.ksprintf log fmt
 
 let effect = EffectBuilder()
 
@@ -32,28 +36,49 @@ let extractResult e =
     | Some v -> v
     | None -> failwith "That effect hasn't finished all of its side effects yet"
 
-let extract e =
+type Eventual<'q, 'input, 't> = Ready of 't | Awaiting of ('q * ('input -> Eventual<'q, 'input, 't>))
+let step e =
     let rec loop accum = function
-        | Result v -> v, List.rev accum
+        | Result v -> Ready(v, List.rev accum)
+        | Read(query, cont: Input -> Effect<_>) ->
+            Awaiting(({| query = query; logOutput = List.rev accum |}), (fun e -> cont e |> loop []))
         | Log(msg, cont) ->
             cont() |> loop (msg::accum)
     loop [] e
+module String =
+    let join (input: string seq) = System.String.Join("\n", input)
+type Capability<'t> = Capability of label: string * (unit -> 't)
+let closure e =
+    match step e with
+    | Ready(v, logOut) -> v.ToString(), []
+    | Awaiting(data, cont) ->
+        let makeCapability (i: int) : Capability<Eventual<_,_,_>> =
+            Capability(i.ToString(), fun () -> i.ToString() |> cont)
+        sprintf "%s\n%s" (String.join data.logOutput) data.query, [1..10] |> List.map makeCapability
+let rec fixedPoint = function
+    | output, [] -> output |> printfn "Value: %s"
+    | output, capabilities ->
+        printfn "%s" output
+        let (Capability(label, cap)) = capabilities |> chooseRandom
+        printfn "Exercising %s" label
+        cap() |> closure |> fixedPoint
 
-let handle e =
-    let rec loop accum = function
-        | Result v -> v, List.rev accum
-        | Log(msg, f) -> loop (msg::accum) (inv f)
-    loop [] e
+
 
 let rec fib = function
     | 0 | 1 -> effect { return 1 }
     | n -> effect {
         let! a = fib (n-1)
         let! b = fib (n-2)
-        do! (log (sprintf "%d(%d) |X| %d(%d) ==> %d(%d)" (n-1) a (n-2) b n (a+b)))
+        do! logf "%d(%d) |X| %d(%d) ==> %d(%d)" (n-1) a (n-2) b n (a+b)
         return (a + b)
         }
 
-fib 10 |> handle
 fib 10 |> extractResult
-fib 10 |> extract
+fib 10 |> closure |> fixedPoint
+match fib 10 |> closure with
+| label, Capability(clabel, cont)::rest ->
+    printfn "%s\n%s" label clabel
+    cont() |> printfn "%A"
+| l, [] -> printfn "%s" l
+
