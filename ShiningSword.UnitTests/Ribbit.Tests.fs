4d7a5cec3c8250f195b305fd86f6359f45173918
Max Wilson <wilson.max@gmail.com>
2020-10-12 00:38:25 -0700

    Towards a ribbit model

--- ShiningSword.UnitTests/Ribbit.Tests.fs
+++ ShiningSword.UnitTests/Ribbit.Tests.fs
@@ -5,6 +5,15 @@ open Expecto
 open Expecto.Flip
 open FsCheck
 
+type Demand = int * string
+type State = Map<int * string, obj>
+type Eventual<'state, 't> = Eventual of ('state -> 'state * Result<'state, 't>)
+and Result<'state, 't> =
+    | Awaiting of Demand list * Eventual<'state, 't>
+    | Ready of 't
+let execute (Eventual(f)) state =
+    f state
+
 [<Tests>]
 let tests = testList "ribbit.unit" [
     testCase "stub" <| fun _ ->
