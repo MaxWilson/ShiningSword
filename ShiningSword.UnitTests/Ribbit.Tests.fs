bb7713288a98da6fff9e5c1d59d00a5b61e75de1
Max Wilson <wilson.max@gmail.com>
2020-10-12 09:42:08 -0700

    Towards a ribbit architecture

--- ShiningSword.UnitTests/Ribbit.Tests.fs
+++ ShiningSword.UnitTests/Ribbit.Tests.fs
@@ -5,14 +5,33 @@ open Expecto
 open Expecto.Flip
 open FsCheck
 
-type Demand = int * string
-type State = Map<int * string, obj>
-type Eventual<'state, 't> = Eventual of ('state -> 'state * Result<'state, 't>)
-and Result<'state, 't> =
-    | Awaiting of Demand list * Eventual<'state, 't>
-    | Ready of 't
-let execute (Eventual(f)) state =
-    f state
+type Key = int * string
+type Logic<'state, 'result> = 'state -> 'state * LogicOutput<'state, 'result>
+and LogicOutput<'state, 'result> = Ready of 'result | Awaiting of Logic<'state, 'result>
+
+type State = {
+    data: Map<Key, obj>
+    blocked: Map<Key, Logic<State, string>>
+    }
+    with static member fresh = { blocked = Map.empty; data = Map.empty }
+
+type Prop<'t> = { name: string }
+let read id (prop: Prop<'t>) (state: State) =
+    match state.data |> Map.tryFind (id, prop.name) with
+    | Some (:? 't as v) -> Some v
+    | _ -> None
+
+let readThen read demand (id: int, prop: Prop<'t>) (thenLogic: 't -> Logic<State, 't>) : Logic<State, 't> =
+    let rec logic state =
+        match read id prop state with
+        | Some v ->
+            thenLogic v state
+        | None ->
+            state |> demand (id, prop.name) logic
+    logic
+
+let demand (id, propName) logic state =
+    { state with blocked = state.blocked |> Map.add (id, propName) logic }
 
 [<Tests>]
 let tests = testList "ribbit.unit" [
