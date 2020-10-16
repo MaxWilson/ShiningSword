696bcd89acd747a96ba4c6da15015df79b2e4aaa
Max Wilson <wilson.max@gmail.com>
2020-10-16 13:42:14 -0700

    Green: spawn now works

--- ShiningSword.UnitTests/Ribbit.Tests.fs
+++ ShiningSword.UnitTests/Ribbit.Tests.fs
@@ -16,8 +16,8 @@ module Queue =
 
 type State = {
     data: Map<Key, obj>
-    blocked: Map<Key, Logic>
-    workQueue: Logic Queue.d
+    blocked: Map<Key, Logic<unit>>
+    workQueue: Logic<unit> Queue.d
     log: string list
     }
     with
@@ -26,53 +26,72 @@ type State = {
         data = Map.empty
         workQueue = Queue.empty
         log = [] }
-and Logic = Logic<State, string>
+and Logic<'t> = Logic<State, 't>
 
 type Prop<'t> = { name: string }
-let read id (prop: Prop<'t>) (state: State) =
-    match state.data |> Map.tryFind (id, prop.name) with
-    | Some (:? 't as v) -> Some v
-    | _ -> None
+module Logic =
+    let read id (prop: Prop<'t>) (state: State) =
+        match state.data |> Map.tryFind (id, prop.name) with
+        | Some (:? 't as v) -> Some v
+        | _ -> None
 
-let readThen read demand (id: int, prop: Prop<'t>) (thenLogic: 't -> Logic<State, 't>) : Logic<State, 't> =
-    let rec logic state =
-        match read id prop state with
-        | Some v ->
-            thenLogic v state
-        | None ->
-            state |> demand (id, prop.name) logic
-    logic
+    let readThen read demand (id: int, prop: Prop<'t>) (thenLogic: 't -> Logic<State, 't>) : Logic<State, 't> =
+        let rec logic state =
+            match read id prop state with
+            | Some v ->
+                thenLogic v state
+            | None ->
+                state |> demand (id, prop.name) logic
+        logic
 
-let demand (id, propName) logic state =
-    { state with blocked = state.blocked |> Map.add (id, propName) logic }
+    let demand (id, propName) logic state =
+        { state with blocked = state.blocked |> Map.add (id, propName) logic }
 
-let fulfill (id, prop: Prop<'t>) (value: 't) state =
-    state
+    let fulfill (id, prop: Prop<'t>) (value: 't) state =
+        state
 
-let spawn log logic state =
-    state
+    let addToQueue logic state =
+        state
 
-let addToQueue logic state =
-    state
+    // Like Task.Continue or Async.Map
+    let continueWith f logic =
+        let rec continueWith logic state =
+            match logic state with
+            | state, Ready v ->
+                f v state
+            | state, Awaiting logic ->
+                state, Awaiting (logic |> continueWith)
+        continueWith logic
 
-let untilFixedPoint state =
-    state
+    let andLog logic =
+        logic |> continueWith (fun msg state -> { state with log = state.log @ [msg] }, Ready ())
 
-type LogicBuilder() =
-    member _.Return x =
-        fun state -> state, Ready x
+    let spawn (logic: Logic<string>) state =
+        match (logic |> andLog) state with
+        | state, Ready () ->
+            state
+        | state, Awaiting logic ->
+            { state with workQueue = Queue.append logic state.workQueue }
 
-let logic = LogicBuilder()
+    let untilFixedPoint state =
+        state
+
+    type LogicBuilder() =
+        member _.Return x =
+            fun state -> state, Ready x
+
+    let logic = LogicBuilder()
+open Logic
 
 [<Tests>]
-let tests = testList "ribbit.unit" [
+let tests = testList "ribbit.scenario" [
     let verifyLog msg state =
         Expect.contains "Missing message" msg state.log
     testCase "stub" <| fun _ ->
         Expect.equal "Placeholder" 42 (RuleEngine.placeholder 40 + 2)
     testCase "Scenario1" <| fun _ ->
         State.fresh
-        |> spawn id (logic {
+        |> spawn (logic {
                 return "abc"
             })
         |> untilFixedPoint
