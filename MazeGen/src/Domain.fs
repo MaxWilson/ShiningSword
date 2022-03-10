c5fe1e4450026dbabd116d1c66d3de46b9894bf3
Max Wilson <maxw@microsoft.com>
2022-03-10 14:51:12 -0800

    Add test and impl for moveTo and connectionTo

--- MazeGen/src/Domain.fs
+++ MazeGen/src/Domain.fs
@@ -1,9 +1,28 @@
 module Domain
 
-type Coord = int * int
+type PointCoord = PointCoord of x: int * y:int
+type Connection = Connection of x: int * y: int
 type Maze = {
     size: int * int
     connections: bool[][]
     }
 
 let newMaze (width, height, initialConnection) = { size = (width, height); connections = Array.init (width+1) (fun _ -> Array.create (height+1) initialConnection) }
+
+type Direction = Up | Down | Left | Right
+
+let moveTo direction (PointCoord(x, y)) =
+    match direction with
+        | Up -> x, y+1
+        | Down -> x, y-1
+        | Left -> x-1, y
+        | Right -> x+1, y
+    |> PointCoord
+
+let connectionTo direction (PointCoord(x, y)) =
+    match direction with
+        | Up -> x, y+1
+        | Down -> x, y
+        | Left -> x, y
+        | Right -> x+1, y
+    |> Connection
--- MazeGen/tests/Tests.fs
+++ MazeGen/tests/Tests.fs
@@ -2,6 +2,7 @@ module Tests
 open Expecto
 open Hedgehog
 open Swensen.Unquote
+open Domain
 
 module Array =
     let every f arr = arr |> Array.exists (not << f) |> not
@@ -13,6 +14,9 @@ let appTests = testList "App tests" [
         Expect.equal result 5 "Result must be 5"
 ]
 
+let chooseFrom inputs = 
+    inputs |> List.map Gen.constant |> Gen.choice
+    
 [<Tests>]
 let allTests = testList "All" [
     appTests
@@ -22,11 +26,20 @@ let allTests = testList "All" [
         property {
             let! w = Range.linear 1 100 |> Gen.int32
             let! h = Range.linear 1 100 |> Gen.int32
-            where (w > 0 && h > 0)
             let! connected = Gen.bool
             let maze = Domain.newMaze(w,h,connected)
             // E.g. a 5 x 5 maze has 6 x 6 possible corridors if you count exits to outside. Otherwise it would be 4 x 4.
             test <@ maze.connections.Length = w + 1 && (maze.connections |> Array.every (fun (row: bool[]) -> row.Length = h + 1)) @>            
+        } |> Property.check
+    testCase "checkNavigationDirections" <| fun _ ->
+        property {
+            let! x = Range.linear 1 100 |> Gen.int32
+            let! y = Range.linear 1 100 |> Gen.int32
+            let reverse = function Up -> Down | Down -> Up | Left -> Right | Right -> Left
+            let! direction = chooseFrom [Up;Down;Left;Right]
+            let startPoint = PointCoord(x,y)
+            let endPoint = moveTo direction startPoint
+            test <@ (connectionTo direction startPoint) = (connectionTo (reverse direction) endPoint) @>
         } |> Property.check
 ]
    
