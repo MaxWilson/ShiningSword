df27a4d61dd5f2a3f0f98a2a999bb888a62af3fd
Max Wilson <maxw@microsoft.com>
2022-03-30 12:22:20 -0700

    Remove Myriadic and old Ribbit

--- ShiningSword.UnitTests/Ribbit.Tests.fs
+++ ShiningSword.UnitTests/Ribbit.Tests.fs
@@ -3,9 +3,6 @@ module Ribbit.Tests
 open Common
 open Expecto
 open FsCheck
-open Domain.Model
-open Domain.Model.Ribbit0
-open Domain.Engine.Ribbit
 #if INTERACTIVE
 #r "nuget: Unquote"
 #endif
@@ -30,41 +27,9 @@ let trimFront (input: string) =
         let prefixN = lines.[0] |> countWhitespace
         lines |> Array.map (skipWhitespace prefixN) |> fun x -> System.String.Join(newLine, x).TrimEnd()
 
-let executeScript script state : RuntimeValue list =
-    notImpl()
-
 [<Tests>]
 let tests = testList "ribbit.scenario" [
-    testCase "Scenario 1" <| fun _ ->
 
-        let ac, sp =
-            withState Game.fresh (state {
-                let! bob = transform1 (Game.add "Bob")
-                do! transform (Game.define "getShieldBonus" [
-                    If(BinaryOp(Dereference(IndirectDataRef("self", "sp")), Const(Number(2)), AtLeast),
-                        Sequence [
-                            Assign(IndirectDataRef("self", "sp"),
-                                        BinaryOp(Dereference(IndirectDataRef("self", "sp")), Const(Number(2)), Minus))
-                            Return (Const (Number 5))
-                        ],
-                        Some(Return (Const (Number 0))))
-                ])
-                let! eventId =
-                    transform1 (
-                      Game.start
-                        [
-                            Assign(LocalRef "__event_1", StartEvent("getShieldBonus", ["self", Const (Id bob)]))
-                            Assign(LocalRef "ac", BinaryOp(Dereference(DataRef(bob, "AC")), Dereference(IndirectEventRef("__event_1")), Plus))
-                            Return (Dereference (LocalRef "ac"))
-                        ] [] |> compile)
-                let api: Ribbit0.Api<_> = { dereference = Game.dereference; defer = Game.defer;
-                             resume = Game.resume; supply = Game.supply; start = Game.startByName }
-                do! transform (supply api (bob, "AC") (Number 18) >> supply api (bob, "sp") (Number 5))
-                let! (g: Game) = get()
-                return ((g.events.[eventId]), g.data.[bob].properties.["sp"])
-            })
-        Expect.equal ac (EventResult (Number 23)) "Bob's AC should compute as 23 since Bob has 5 SP remaining"
-        Expect.equal sp (Number 3) "Bob should have expended 2 of his remaining 5 SP"
     testCase "Lemma 1" <| fun _ ->
         let x = """
             abc
@@ -78,21 +43,5 @@ let tests = testList "ribbit.scenario" [
         ("abc\ndef\n   hij\n   k\nab\n a", "Should trim first line and prefixes evenly")
         ||> Expect.equal (trimFront x)
 
-    testCase "Scenario 2" <| fun _ ->
-        let g = Game.fresh |> executeScript """
-            define getShieldBonus:
-                if self.sp at least 2
-                  self.sp loses 2
-                  return 5
-                else return 0
-
-            add bob
-            let ac = bob.AC + getShieldBonus with self = bob
-            bob.AC = 18
-            bob.sp = 5
-            ac
-            """
-        test <@ g |> List.last = Number 23 @>
-        ()
     ]
 
--- src/ShiningSwordOriginal/Script1.fsx
+++
@@ -1,7 +0,0 @@
-#I __SOURCE_DIRECTORY__
-#r "System.Net.Http"
-#r "NewtonSoft.Json"
-#load @"Common.fs"
-#load @"Abstractions\Parsing.fs"
-
-
