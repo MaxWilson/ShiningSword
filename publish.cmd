9cadd6e40e7f8f6bb46e161e4ebb3791954d212d
Max Wilson <maxw@microsoft.com>
2022-04-01 09:36:25 -0700

    Fix bug: Icons not being published to github

--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,1 @@
-npm run build && git push . head:publish -f && git checkout publish && move /Y dist\* . && git add . && git commit -m publish && git push origin publish -f && git checkout main
+npm run build && git push . head:publish -f && git checkout publish && move /Y dist\* . && mkdir img && move /Y dist\img\* .\img && git add . && git commit -m publish && git push origin publish -f && git checkout main
