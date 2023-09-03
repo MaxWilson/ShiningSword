19301cfd1b51bb545c282375071115610d510bb3
Max Wilson <wilson.max@gmail.com>
2023-09-03 10:40:59 -0700

    Remove test stub for no-longer-needed test. We don't need a special abstraction to represent addressing deep into the CQRS. We can just pass in the creature/object name or id.

--- ShiningSword.UnitTests/POCs.fs
+++ ShiningSword.UnitTests/POCs.fs
@@ -1,37 +1,36 @@
 module POCs
 
-open Expecto
 open Swensen.Unquote
+let verify = Swensen.Unquote.Assertions.test
+open Expecto
 
 open POC
 
 [<Tests>]
-let POCTests() = testList "POCs" [
+let Tests() = testList "POCs" [
     testList "CQRS" [
         testCase "Number test" <| fun () ->
             let test1 = CQRS.Create(0, fun f n -> f n)
-            test <@ test1.State = 0 @>
+            verify <@ test1.State = 0 @>
             let zeroish = test1.Checkpoint()
             test1.Execute ((+) 3)
-            test <@ test1.State = 3 @>
+            verify <@ test1.State = 3 @>
             let threeish= test1.Checkpoint()
             test1.Execute ((*) 2)
-            test <@ test1.State = 6 @>
+            verify <@ test1.State = 6 @>
             let sixish = test1.Checkpoint()
             test1.Rewind threeish
-            test <@ test1.State = 3 @>
+            verify <@ test1.State = 3 @>
             test1.Rewind zeroish
-            test <@ test1.State = 0 @>
+            verify <@ test1.State = 0 @>
             test1.FastForward sixish
-            test <@ test1.State = 6 @>
+            verify <@ test1.State = 6 @>
             test1.Rewind threeish
             test1.Rewind threeish // should be idempotent
-            test <@ test1.State = 3 @>
+            verify <@ test1.State = 3 @>
             test1.Execute ((+) 4)
             test1.Execute ((-) 15)
-            test <@ test1.State = 8 @>
+            verify <@ test1.State = 8 @>
             raises <@ test1.FastForward sixish @> // should throw
-        ptestCase "Nested events" <| fun () ->
-            ()
         ]
     ]
