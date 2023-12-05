3d9dee909c5446ce4ff326a1b202b11fd2e9a3d0
Max Wilson <wilson.max@gmail.com>
2023-12-05 01:00:45 -0800

    First effort: horribly wrong UI for notional swashbuckler.
    A multi-choice chooseN isn't necessarily just container for chooseOnes. The relationship between 1 and N is more subtle than that.

--- src/UI/DFRPG/Chargen.fs
+++ src/UI/DFRPG/Chargen.fs
@@ -2,32 +2,32 @@ module UI.DFRPG.Chargen
 
 type Stuff = Foo | Bar
 type Weapon = Sword | Bow
-type Trait = WeaponMaster of Weapon | CombatReflexes | Skill of string * int
-
-type Constraint = unit -> bool // TODO: find the right type
-let budget(points): Constraint = notImpl()
-let chooseN(n): Constraint = notImpl()
+type Trait = WeaponMaster of Weapon | CombatReflexes | Skill of string * bonus:int
 
 type Offer = Offer of Trait | OneOf of Offer list
     with
-    static member skill(name, points) = Offer(Skill(name, points))
-    static member skill(name, pointRange) = pointRange |> List.map (fun points -> Offer(Skill(name, points))) |> OneOf
+    static member skill(name, bonus) = Offer(Skill(name, bonus))
+    static member skill(name, bonusRange) = bonusRange |> List.map (fun bonus -> Offer(Skill(name, bonus))) |> OneOf
     static member either(offerings) = offerings |> OneOf
 
-type Offers(constrain: Constraint, offers) =
+type Offers = ChooseN of int * Offer list | ChooseBudget of int * Offer list
+    with
     member this.renderedOfferings() = notImpl()
 
 let swash() = [
-    Offers(budget 20, [
-        Offer.skill("Acrobatis", 2)
-        Offer.skill("Acrobatis", [1;2;4])
+    ChooseBudget(20, [
+        Offer.skill("Acrobatics", 2)
+        Offer.skill("Acrobatics", [1..3])
         Offer.either([
             Offer.skill("Rapier", 20)
             Offer.skill("Broadsword", 20)
             ])
         ])
-    Offers(notImpl "Need to represent the choices for a swashbuckler, with constraints and nesting and budgets such", notImpl())
     ]
 
-let init _ = Foo
+type Model = {
+    template: Offers list
+    }
+
+let init _ = { template = swash() }
 let update msg1 model = model
--- src/UI/DFRPG/ChargenView.fs
+++ src/UI/DFRPG/ChargenView.fs
@@ -5,4 +5,27 @@ open UI.DFRPG.Chargen
 [<ReactComponent>]
 let View() =
     let model, dispatch = React.useElmishSimple init update
-    Html.div "placeholder"
+
+    Html.div [
+        let rec renderOffer = function
+            | OneOf offers ->
+                Html.ul [
+                    for offer in offers do
+                        Html.li [prop.children [renderOffer offer]]
+                ]
+            | Offer offer ->
+                Html.text (offer.ToString())
+        let rec renderOffers = function
+            | ChooseBudget (points, offers) ->
+                Html.div [
+                    Html.div [prop.text $"Choose {points} points:"]
+                    for offer in offers do renderOffer offer
+                ]
+            | ChooseN (n, offers) ->
+                Html.div [
+                    Html.div [prop.text $"Choose {n}"]
+                    for offer in offers do renderOffer offer
+                ]
+        for offers in model.template do
+            renderOffers offers
+        ]
