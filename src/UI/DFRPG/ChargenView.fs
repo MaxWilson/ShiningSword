7b7912bc49befbac8ff62751fce70d97b1846b14
Max Wilson <wilson.max@gmail.com>
2023-12-04 23:46:49 -0800

    Should be a React component to avoid error about changing number of hooks in Router component

--- src/UI/DFRPG/ChargenView.fs
+++ src/UI/DFRPG/ChargenView.fs
@@ -1,6 +1,8 @@
 module UI.DFRPG.Chargen.View
 open Feliz
 open UI.DFRPG.Chargen
+
+[<ReactComponent>]
 let View() =
     let model, dispatch = React.useElmishSimple init update
     Html.div "placeholder"
