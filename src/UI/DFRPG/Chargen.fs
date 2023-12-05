5774fbfeb2864e16eadc86f958418449ff2fecb7
Max Wilson <wilson.max@gmail.com>
2023-12-04 23:49:59 -0800

    wip

--- src/UI/DFRPG/Chargen.fs
+++ src/UI/DFRPG/Chargen.fs
@@ -1,6 +1,12 @@
 module UI.DFRPG.Chargen
 
 type Stuff = Foo | Bar
+type Weapon = Sword | Bow
+type Trait = WeaponMaster of Weapon | CombatReflexes | Skill of string * int
+
+let swash() = [
+    notImpl "Need to represent the choices for a swashbuckler, with constraints and nesting and budgets such"
+    ]
+
 let init _ = Foo
 let update msg1 model = model
-
