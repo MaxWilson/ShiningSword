4617c42155f702a95852112628957823901fb5a6
Max Wilson <wilson.max@gmail.com>
2023-12-27 17:26:41 -0800

    Sketch requirements

--- test/Chargen.Accept.fs
+++ test/Chargen.Accept.fs
@@ -9,6 +9,17 @@ type MenuOutput =
   | Leveled of string * int
   | Leaf of string
 
+(* Requirements:
+Terseness: flatten some and's, e.g. "Fast draw (swords & daggers) +1" all on one line, instead of two separate lines.
+Terseness: hide irrelevant options in either, e.g. if you can pick swords or daggers and sword is picked, don't show dagger any more.
+I.e. collapse either when semi-ready (no more choices at that level).
+Terseness: don't show child options until it's possible to pick them, e.g. don't show specific kinds of swords until sword! is picked.
+Correctness: checkboxes only inside an either or budget
+Terseness: hide things too expensive for remaining budget
+UX: leveled traits
+Correctness: mutual exclusion within either unselects old selection when new selection is made
+*)
+
 // swash is not a MenuOutput but it can create MenuOutputs which can then be either unit tested or turned into ReactElements
 // think of swash as an offer menu
 // let swash = [
@@ -31,12 +42,17 @@ type MenuOutput =
 //         ]
 //     ]
 
-
-
 [<Tests>]
 let tests =
-  testList "Acceptance" [
-    testCase "placeholder" <| fun _ ->
-      test <@ 1 + 1 = 2 @>
+    testList "Acceptance" [
+        testCase  "Terseness #1" <| fun () -> failtest """flatten some and's, e.g. "Fast draw (swords & daggers) +1" all on one line, instead of two separate lines."""
+        testCase  "Terseness #2" <| fun () -> failtest """hide irrelevant options in either, e.g. if you can pick swords or daggers and sword is picked, don't show dagger any more. I.e. collapse either when semi-ready (no more choices at that level)."""
+        testCase  "Terseness #3" <| fun () -> failtest """don't show child options until it's possible to pick them, e.g. don't show specific kinds of swords until sword! is picked."""
+        testCase "Correctness #1" <| fun () -> failtest """checkboxes only inside an either or budget"""
+        testCase  "Terseness #4" <| fun () -> failtest """hide things too expensive for remaining budget"""
+        testCase  "UX #1" <| fun () -> failtest """leveled traits"""
+        testCase "Correctness #2" <| fun () -> failtest """mutual exclusion within either unselects old selection when new selection is made"""
+        testCase "placeholder" <| fun _ ->
+            test <@ 1 + 1 = 2 @>
 
-  ]
+    ]
