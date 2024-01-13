7a3b09c99ace0befad9abbc822b4bea721aacfa8
Max Wilson <wilson.max@gmail.com>
2024-01-13 09:35:53 -0800

    (still red) wip on test issues

--- test/Chargen.Accept.fs
+++ test/Chargen.Accept.fs
@@ -100,6 +100,13 @@ let units = testList "Unit.Chargen" [
     testCase "basic either" <| fun () ->
         either[trait' Fight; trait' Hide] |> testFor [] (Either(None, [false, key "Fight", Leaf "Fight"; false, key "Hide", Leaf "Hide"]))
         either[trait' Fight; trait' Hide] |> testFor ["Fight"] (Either(None, [true, key "Fight", Leaf "Fight"]))
+    testCase "and' should have a key based on its children" <| fun() ->
+        let offer = skill("Fast-Draw (Sword)", +1)
+        test <@ offer.LabelWhenUnselected 0 = "Fast-Draw (Sword) +1" @>
+        let offer = and'([skill("Fast-Draw (Sword)", +1); skill("Fast-Draw (Dagger)", +1)])
+        test <@ offer.LabelWhenUnselected 0 = "Fast-Draw (Sword) +1 and Fast-Draw (Dagger) +1" @>
+        let offer = eitherN[and'([skill("Fast-Draw (Sword)", +1); skill("Fast-Draw (Dagger)", +1)]); skill("Stealth", +1) |> Op.promote]
+        offer |> testFor ["Stealth +1"] (Either(None, [true, key "Stealth +1", Leaf "Stealth +1"]))
     testCase "nested either with list" <| fun () ->
         let nestedEither = eitherN [
             either(labelConfig "Sword!", [skillN("Rapier", [+5..+6]); skillN("Broadsword", [+5..+6]); skillN("Shortsword", [+5..+6])]) |> promote // make sure to exercise Op.level even though the actual DFRPG swashbuckler doesn't have a +6 option
@@ -147,11 +154,6 @@ let units = testList "Unit.Chargen" [
                         ])
                     ])
             )
-    testCase "and' should have a key based on its children" <| fun() ->
-        let offer = skill("Fast-Draw (Sword)", +1)
-        test <@ offer.LabelWhenUnselected 0 = "Fast-Draw (Sword) +1" @>
-        let offer = and'([skill("Fast-Draw (Sword)", +1); skill("Fast-Draw (Dagger)", +1)])
-        test <@ offer.LabelWhenUnselected 0 = "Fast-Draw (Sword) +1 and Fast-Draw (Dagger) +1" @>
     ]
 [<Tests>]
 let tests =
